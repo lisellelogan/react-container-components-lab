f631fb6f5447e34a2324a21f2d187672bb8bfb2f
lisellelogan <liselle.logan@yahoo.com>
2021-06-03 14:29:20 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
-// Code MovieReviews Here
+// Your MovieReviews component should be stateless and functional.
+
+// You are free to lay out each review as you like using the data that the API returns, but make sure that the component outputs a top-level element with the class review-list and that each review is wrapped by an element with class review.
+
+import React from 'react'
+
+const MovieReviews = ({ reviews }) => {
+
+    const renderReviews = () => {
+        return reviews.map((review, index) => {    
+            return <li key={index}>{review.display_title}: {review.summary_short}</li>
+        })
+    }
+    
+    return (
+        <div className="review-list">
+            <ul>
+                {renderReviews()}
+            </ul>
+        </div>
+    )
+}
+
+export default MovieReviews
